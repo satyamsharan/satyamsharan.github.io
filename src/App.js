fd92ee02aec114d2ea9de8462bf61ea95eedbe56
Satyam Sharan <satyam.sharan@afry.com>
2023-04-25 11:28:50 +0200

    Updated page

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Under maintenance. Please check back tomorrow. 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/satyamsharan/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn Profile
         </a>
       </header>
     </div>
